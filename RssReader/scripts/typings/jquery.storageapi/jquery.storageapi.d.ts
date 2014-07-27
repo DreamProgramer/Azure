dc2d8dd5364b6f62c914865225f36661d9a00569
mazi <champion.is.acmilan@gmail.com>
2014-07-27 14:25:55 +0900

    make rss reader file

---
+++ RssReader/scripts/typings/jquery.storageapi/jquery.storageapi.d.ts
@@ -0,0 +1,38 @@
+﻿/**
+ * Definitions by: yoshiyuki Taniguchi
+ */
+
+interface JQueryStorageConfig {
+    path?: string;
+    expires?: string;
+    domain?: string;
+}
+interface JQueryStorage {
+    get<T>(...keys: string[]): T;
+    get(keys: string[]): any;
+    set<T>(key: string, value: T): void;
+    set(...keysAndValue: any[]): void;
+    set(dict: any): void;
+    keys(...keys: string[]): string[];
+    isEmpty(...keys: string[]): boolean;
+    isEmpty(keys: string[]): boolean;
+    isSet(...keys: string[]): boolean;
+    isSet(keys: string[]): boolean;
+    remove(...keys: string[]): void;
+    remove(keys: string[]): void;
+    removeAll(global?: boolean): void;
+    
+}
+interface JQueryCookieStorage extends JQueryStorage{
+    setExpires(expires: string): JQueryCookieStorage;
+    setPath(path: string): JQueryCookieStorage;
+    setDomain(domain: string): JQueryCookieStorage;
+    setConf(config: JQueryStorageConfig): JQueryCookieStorage;
+    setDefaultConf(): JQueryCookieStorage;
+}
+interface JQueryStatic {
+    localStorage: JQueryStorage;
+    sessionStorage: JQueryStorage;
+    cookieStorage: JQueryCookieStorage;
+    initNamespaceStorage(name: string): JQueryStatic;
+}
